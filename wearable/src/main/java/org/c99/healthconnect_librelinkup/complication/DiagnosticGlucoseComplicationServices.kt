114b545c28c3b15f71a7c657b5fd9b3b87e18fc4
omega015 <21293424+omega015@users.noreply.github.com>
2026-09-15 20:52:44 +0100

    Replace diagnostic providers with final complication variants [skip ci]

--- wearable/src/main/java/org/c99/healthconnect_librelinkup/complication/DiagnosticGlucoseComplicationServices.kt
+++ wearable/src/main/java/org/c99/healthconnect_librelinkup/complication/DiagnosticGlucoseComplicationServices.kt
@@ -1,11 +1,11 @@
-/*
- * Diagnostic complication providers used to test Samsung watch-face filtering.
- */
-
 package org.c99.healthconnect_librelinkup.complication
 
-class ShortGlucoseComplicationService : GlucoseComplicationService()
-
-class RangeGlucoseComplicationService : GlucoseComplicationService()
+class GlucoseTrendComplicationService : GlucoseComplicationService() {
+    override val includeTrendInText: Boolean = true
+}
 
 class BigGlucoseComplicationService : GlucoseComplicationService()
+
+class BigGlucoseTrendComplicationService : GlucoseComplicationService() {
+    override val includeTrendInImage: Boolean = true
+}
